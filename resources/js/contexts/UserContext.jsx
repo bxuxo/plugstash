9299fcb7537e162585341c2ac2fd3696de9903f8
kp2129 <kristapspolis0@gmail.com>
2023-12-20 08:46:46 +0200

    'jeje'

--- resources/js/contexts/UserContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext( {
-    userID: null,
-    setUserID: ( ) => { },
-    username: null,
-    setUsername: ( ) => { }
-} );
-
