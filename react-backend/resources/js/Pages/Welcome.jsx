10858a605d63f162d5e4337392e1a922301ecf13
kp2129 <kristapspolis0@gmail.com>
2024-01-08 14:31:24 +0200

    'll'

--- react-backend/resources/js/Pages/Welcome.jsx
+++ react-backend/resources/js/Pages/Welcome.jsx
@@ -20,6 +20,13 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     >   
                                     Users
                                 </Link>
+                                <Link
+                                    href={route('createUser')}
+                                    data={{ auth }}
+                                    className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                    >   
+                                    Create User
+                                </Link>
                            
                                 <Link
                                     href={route('dashboard')}
